92763c8b8e8ad669ad9b999e734d91414ba85e20
Ramiro <ramarivera@gmail.com>
2016-11-09 19:16:31 -0300

    Agregado maven a la base

--- alpine/storm-nimbus/start-supervisor.sh
+++ alpine/storm-nimbus/start-supervisor.sh
@@ -4,7 +4,7 @@ export IP=`hostname -i`
 
 ZOOK_IP=$(getent hosts zook | awk '{ print $1 }')
 
-sed -i -e "s/%zookeeper%/$ZOOK_IP/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%zookeeper%/zook/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus%/$IP/g" $STORM_HOME/conf/storm.yaml
 
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
--- alpine/storm/start-supervisor.sh
+++ alpine/storm/start-supervisor.sh
@@ -1,11 +1,8 @@
 ZOOK_IP=$(getent hosts zook | awk '{ print $1 }')
 NIMB_IP=$(getent hosts nimb | awk '{ print $1 }')
 
-sed -i -e "s/%zookeeper%/$ZOOK_IP/g" $STORM_HOME/conf/storm.yaml
-sed -i -e "s/%nimbus%/$NIMB_IP/g" $STORM_HOME/conf/storm.yaml
-
-
-
+sed -i -e "s/%zookeeper%/zook/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%nimbus%/nimb/g" $STORM_HOME/conf/storm.yaml
 
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
 
